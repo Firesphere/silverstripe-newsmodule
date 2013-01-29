c290a366982f289069deabc445f072b5b575d596
Simon Erkelens <simon@casa-laguna.net>
2013-01-30 00:11:44 +0100

    Update javascript/news-left.js

--- javascript/news-left.js
+++ javascript/news-left.js
@@ -1,27 +1,3 @@
 /**
- * on load, submit the empty searchform to get default results list instead of 'empty' screen
+ * This, does, nothing. It's old 2.4 code.
  */
-jQuery(function($) {
-    if ($('#SearchForm_holder .tabstrip').length > 0) {
-        /**
-         * tabstrip may not be initialized when this is executed
-         */
-        // the initial search
-        var activeTab = ($('#SearchForm_holder .tabstrip li:first a:first').attr('href')).replace(/^.*#/, '');
-        $('#Form_Search' + activeTab).submit();
-
-        // the onchange search
-        $('#SearchForm_holder .tabstrip li a').live('click', function() {
-            var activeTab = ($(this).attr('href')).replace(/^.*#/, '');
-            $('#Form_Search' + activeTab).submit();
-        });
-    } else if ($('#ModelClassSelector').length > 0) {
-        // the initial search
-        $('#Form_Search' + $('#ModelClassSelector select').val()).submit();
-
-        // the onchange search
-        $('#ModelClassSelector select').change(function(){
-            $('#Form_Search' + $('#ModelClassSelector select').val()).submit();
-        });
-    }
-});
