4d386097b08afe8b5603adc96922984184a943fc
Simon <simon@casa-laguna.net>
2013-02-15 19:41:21 +0100

    Issue #8

--- _config.php
+++ _config.php
@@ -7,3 +7,10 @@ Object::add_extension('ContentController', 'NewsExtension');
 Object::add_extension('SiteConfig', 'NewsSiteConfigDecorator');
 
 SSAkismet::setAPIKey('YOURAPIKEY');
+
+/**
+ * Remove or comment the following lines to disable features.
+ */
+ShortcodeParser::get()->register('tweet',array('NewsHolderPage','TweetHandler'));
+ShortcodeParser::get()->register('code',array('NewsHolderPage','GeshiParser'));
+ShortcodeParser::get()->register('YT',array('NewsHolderPage','YouTubeHandler'));
