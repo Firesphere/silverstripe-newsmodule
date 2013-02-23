743a2a0df0499e9ed243720a0bc8de42da1cb288
Simon <simon@casa-laguna.net>
2013-02-23 14:56:50 +0100

    Better getCMSFields

--- code/objects/Tag.php
+++ code/objects/Tag.php
@@ -52,7 +52,6 @@ class Tag extends DataObject {
 	
 	public function getCMSFields() {
 		$fields = FieldList::create(TabSet::create('Root'));
-
 		$fields->addFieldsToTab('Root.Main', 
 			array(
 				$text = TextField::create('Title', _t($this->class . '.TITLE', 'Title')),
