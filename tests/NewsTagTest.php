684577068999e264d860c94e0c6fe1c984df99a5
Firesphere <simon@casa-laguna.net>
2014-04-01 12:49:37 +0200

    setup for tag-testing.

---
+++ tests/NewsTagTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Unittests for tags.
+ * Tags should be linked to multiple items, but not all, and vice-versa.
+ *
+ * @author Simon 'Sphere' Erkelens
+ */
+class NewsTagTest extends SapphireTest {
+	
+	protected static $fixture_file = 'NewsTest.yml';
+
+}
